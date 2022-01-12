036c49cdda8be1f4776c223adbd513f7b1199cb2
Abdulrahman Hamad <wadaln3ma@gmail.com>
2022-01-12 22:47:12 +0200

    initial commit
    Started the project and completed the the navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import './Navbar.css'
+import { NotificationsNone, Language, Settings } from '@material-ui/icons'
+
+import logo from '../../images/logo.svg'
+
+const Navbar = ()=>{
+  return(
+    <div className="top-nav">
+    <div className="nav-wrapper">
+      <div className="nav-left">
+        <span className="logo"
+        >
+        Dashboard
+        </span>
+      </div>
+      
+      <div className="nav-right">
+        <div className="icons-container">
+          <NotificationsNone />
+          <span className="icon-badge">2</span>
+        </div>
+        <div className="icons-container">
+          <Language />
+          <span className="icon-badge">2</span>
+        </div>
+        <div className="icons-container">
+          <Settings />
+        </div>
+        <img src={ logo }
+        alt="avatar"
+        className="avatar" />
+      </div>
+    </div>
+    </div>
+)}
+  
+
+export default Navbar
