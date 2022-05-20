0377afd593c22c91f6dda08fa0e98ff76b4cc70a
Abdulrahman Hamad <wadaln3ma@gmail.com>
2022-05-20 16:51:03 +0300

    Add Widgets

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,9 @@
 import FeaturedInfo from './featuredInfo/FeaturedInfo.jsx'
 import Chart from '../chart/Chart'
 import { userData } from './data'
+import WidgetSm from "../../components/widgetSm/WidgetSm"
+import WidgetLg from "../../components/widgetLg/WidgetLg"
+
 
 const Home = ()=>{
   return (
@@ -9,6 +12,10 @@ const Home = ()=>{
 
       <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
 
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
       
     </div>
 )}
